560b6afd43a2f03f7275b181ba435abb7380a2ab
millkun <147944172+millkun@users.noreply.github.com>
2025-06-04 17:58:08 +0300

    Delete speech-recognition-efm32 directory

--- speech-recognition-efm32/src/audio_processing/dct.py
+++
@@ -1,7 +0,0 @@
-def compute_dct(audio_data):
-    from scipy.fftpack import dct
-    return dct(audio_data, type=2, norm='ortho')
-
-def compute_idct(dct_data):
-    from scipy.fftpack import idct
-    return idct(dct_data, type=2, norm='ortho')
